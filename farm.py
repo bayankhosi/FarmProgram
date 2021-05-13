fb0026d99edf1a262e67dba68959647e4762529c
Bayanda Dlamini <bayankhosi@gmail.com>
2021-05-13 17:35:44 +0200

    UI improvement

--- farm.py
+++ farm.py
@@ -13,12 +13,12 @@ population = whole.cell(row=2, column=month + 1).value
 pig_id = individual['Z1'].value
 
 
-def buy_age():      # option to check current age
-    age_bought = int(input("Age of piglet(weeks): "))
+def buy_age():      # function for entering piglets
+    age_bought = int(input("\nEnter Age of piglet (weeks): "))
     date_born = purchase_date - datetime.timedelta(days=7 * age_bought)
     individual.cell(row=rw, column=2).value = date_born
 
-    purchase_price = int(input("Enter purchase price: "))
+    purchase_price = int(input("\nEnter purchase price: "))
     individual.cell(row=rw, column=3).value = purchase_price
 
     # print("Population: ", population)  # , '\n', "Piglet born: ",date_born)
@@ -26,26 +26,26 @@ def buy_age():      # option to check current age
     return
 
 
-def consumables():  # kak spent on well being
+def consumables():  # resources spent on well being
     """ Record:
             population each month
             average age each month
             feed each month """
 
-    consumable_choice = int(input("1.Feed   2.Miscelleneous: "))
+    consumable_choice = int(input("\nWhich Consumable are you recording?\n1.Feed   2.Miscelleneous: "))
     if consumable_choice == 1:
-        print("Enter amount of feed bought (Kg)")
+        print("\nEnter amount of feed bought (Kg)")
         feed_weight = int(input()) + whole.cell(row=3, column=month+1).value
         # record the amount
         whole.cell(row=3, column=month+1).value = feed_weight
 
-        print("Enter price of feed bought (E)")
+        print("\nEnter price of feed bought (E)")
         feed_price = int(input()) + whole.cell(row=4, column=month+1).value
         whole.cell(row=4, column=month+1).value = feed_price
         # av feed per pig
         # av feed per pig per pig weight
     elif consumable_choice == 2:
-        print("Enter price of item")
+        print("\nEnter price of item (E)")
         misc_price = int(input()) + whole.cell(row=5, column=month+1).value
         whole.cell(row=5, column=month+1).value = misc_price
 
@@ -55,42 +55,46 @@ def sale():
     # make averages for that individual pig available
     # profit on the pig by subtracting av spend on it
     individual.cell(row=rw, column=5).value = slaughter_weight
-    price_Kg = float(input("Price per Kg: "))
+    price_Kg = float(input("\nPrice per Kg: "))
     sale_price = slaughter_weight * price_Kg
     individual.cell(row=rw, column=7).value = sale_price
 
-    print("Population: ", population)
+    print("\nNew Population: ", population)
 
 
 def monitor():
-    View = int(input("1. Individual   2. Whole Month Data :   "))
+    View = int(input("\nView data for: \n1. Individual Pig   2. Whole Month Data : "))
     if View == 1:
         # find age
-        pig_id = int(input("Enter ID of pig you wanna view: "))
-        print("Purchase Date: ", datetime.datetime.date(individual.cell(
+        pig_id = int(input("\nEnter ID of pig you want to view: "))
+        print("\nPurchase Date: ", datetime.datetime.date(individual.cell(
             row=pig_id + 1, column=2).value))
 
-        print("Purchase Price: ", individual.cell(
+        print("\nPurchase Price: ", individual.cell(
             row=pig_id + 1, column=3).value)
 
         date_born = datetime.datetime.date(
             individual.cell(row=pig_id + 1, column=2).value)
-        print("Age:  ", (today-date_born).days, "days")
+        print("\nAge:  ", (today-date_born).days, "days")
 
     elif View == 2:
-        print("Current population:   ", whole.cell(
+        print("\nCurrent population:   ", whole.cell(
             row=2, column=month + 1).value)
-        print("Feed Mass Bought:   ", whole.cell(
+        print("\nFeed Mass Bought:   ", whole.cell(
             row=3, column=month + 1).value)
-        print("Price of feed:  ", whole.cell(row=4, column=month + 1).value)
-        print("Total spent:  ", whole.cell(row=4, column=month +
+        print("\nPrice of feed:  ", whole.cell(row=4, column=month + 1).value)
+        print("\nTotal spent:  ", whole.cell(row=4, column=month +
               1).value + whole.cell(row=5, column=month + 1).value)
 
 
 while loop == 2:
-    print("What action are you recording?", '\n',
-          "1. Bought Piglet 2. Bought Consumable 3. Sale 4. View Data")
-
+    print("""************************************************************************
+            These are the operations that can be performed\n
+            [1] - Record New Piglet
+            [2] - Record Bought Consumable(s)
+            [3] - Record Slaughter and Sale
+            [4] - View Data
+        """)
     action = int(input())
 
     if action == 1:
@@ -103,7 +107,7 @@ while loop == 2:
         individual['Z1'].value = pig_id
         rw = pig_id + 1
         individual.cell(row=rw, column=1).value = pig_id
-        print("The pig's ID is: ", pig_id)
+        print("\nThe pig's ID is: ", pig_id)
 
         purchase_date = today         # code to record date
         buy_age()
@@ -117,17 +121,17 @@ while loop == 2:
         # to ensure nxt mnt pop not 0
         whole.cell(row=2, column=month + 2).value = population
 
-        pig_id = int(input("Enter Pig ID: "))
+        pig_id = int(input("\nEnter ID of Pig Slaughtered: "))
         rw = pig_id + 1
 
         slaughter_date = today
         individual.cell(row=rw, column=4).value = slaughter_date
-        slaughter_weight = float(input("Slaughter Weight of pig: "))
+        slaughter_weight = float(input("\nEnter Slaughter Weight of pig: "))
         sale()
 
     elif action == 4:
         monitor()
 
     spread.save('spread.xlsx')
-    loop = int(input("1. Exit,  2. Enter Something else: ",))
-    print("---------------------------------------------------------------------------------------------------------")
+    loop = int(input("\n1. Exit, 2. For Other Operation: ",))
+    print("************************************************************************")
