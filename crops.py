5e930e7c547d9bafc49e6fa9c2ed3ac84f312ecf
Bayanda Dlamini <bayankhosi@gmail.com>
2021-10-30 09:15:22 +0200

    init

---
+++ crops.py
@@ -0,0 +1,46 @@
+from calendar import monthrange
+import datetime
+import openpyxl as opx
+import pandas as pd
+import upload
+import statscalc
+
+fmt = '%d/%m/%Y'
+today = datetime.datetime.now().date()
+month = datetime.datetime.now().month                           # month number
+year = datetime.datetime.now().year                             # year
+
+Crops = opx.load_workbook('./Files/crops.xlsx')
+
+# opens current year sheet
+crops = Crops.worksheets[0]
+#fields = Crops.worksheets[year - 2020]
+
+
+def planting():
+
+    crop = str(input("Enter name of crop (small letters):\n\t"))
+    crops.cell(row=2, column=1).value = crop
+
+    field = str(input("Enter field number:\n\t"))
+    crops.cell(row=2, column=2).value = field
+
+    # date
+    crops.cell(row=2, column=3).value = today.strftime(fmt)
+
+    age = int(input("Enter seedling age in weeks:\n\t"))
+    crops.cell(row=2, column=4).value = 7*age
+
+
+def harvest():
+
+    field = int(input("Enter field number harvested:\n\t"))
+    for i in crops.rows:
+        if (crops.cell(row=2, column=2).value == field) and (crops.cell(row=2, column=9).value == None):
+            print('kak')
+
+
+harvest()
+#print(crops.cell(row=2, column=9).value)
+# planting()
+# Crops.save('./Files/crops.xlsx')
